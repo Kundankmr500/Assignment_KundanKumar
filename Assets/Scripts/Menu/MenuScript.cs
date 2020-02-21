886e9f31c92ccf59fe4a885e5bda06b40aa23d24
Kundan kumar <30592698+Kundankmr500@users.noreply.github.com>
2020-02-21 16:51:49 +0530

    Initiate Project Files

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void GoToNextScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
